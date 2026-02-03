ef300af7316c15383c0be15535715864e7cf444c
Pedroou <pedro.g.ourique@gmail.com>
2026-02-03 17:12:51 -0300

    Github Pages setup

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { ContentProvider } from './context/ContentContext';
 import PublicView from './components/PublicView';
 import AdminLogin from './pages/AdminLogin';
@@ -8,13 +8,13 @@ import AdminDashboard from './pages/AdminDashboard';
 const App: React.FC = () => {
   return (
     <ContentProvider>
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<PublicView />} />
                 <Route path="/admin" element={<AdminLogin />} />
                 <Route path="/admin/dashboard" element={<AdminDashboard />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </ContentProvider>
   );
 };
